08b9d56a3ac0cb28f12fc26a87a2c7ef7107bd9f
p0syd0n <114946695+p0syd0n@users.noreply.github.com>
2023-12-07 13:54:23 +0000

    OPTIMIZATION HIGH

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const headers = {
   'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0",
   'X-Forwarded-For': "cheesemoose"
 }
+const title = 'Goombing Games'
+const urlMap = {'1v1-lol': 'https://topvazstore.github.io/g/1v1-lol', 'bomb-it-7': 'https://topvazstore.github.io/g/bomb-it-7', 'doom': 'https://raz0red.github.io/webprboom/', 'half-life': 'https://pixelsuft.github.io/hl/xash.html#150', 'moto-x3m-winter': 'https://topvazstore.github.io/g/moto-x3m-winter', 'moto-x3m': 'https://topvazstore.github.io/g/moto-x3m', 'password-game': 'https://neal.fun/password-game/', 'quake': 'https://netquake.io', 'realmz': 'https://erth2.party/', 'slope': 'https://slope-p0syd0n.vercel.app/'}
 
-
+const sourceMap = {'geometry-dash': 'geometry-dash'}
 
 //begin server configs
 app.use(express.urlencoded({ extended: true }));
@@ -28,45 +30,22 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('main');
+  res.render('main', {urlMap: urlMap, sourceMap: sourceMap});
 });
 
-app.get('/halflife', (req, res) => {
-  res.render('halflife');
-});
+for (const [key, value] of Object.entries(urlMap)) {
+  app.get(`/${key}`, (req, res) => {
+    res.render('iframe-template', { url: value , title: title+': '+key});
+  });
+}
 
-app.get('/doom', (req, res) => {
-  res.render('doom');
-});
-
-app.get('/slope', (req, res) => {
-  res.render('slope');
-});
-
-app.get('/geometry_dash', (req, res) => {
-  res.render('geometry_dash');
-});
-
-app.get('/realmz', (req, res) => {
-  res.render('realmz');
-});
-
-app.get('/1v1-lol', (req, res) => {
-  res.render('1v1-lol');
-});
-
-app.get('/moto-x3m', (req, res) => {
-  res.render('moto-x3m');
-});
-
-app.get('/moto-x3m-winter', (req, res) => {
-  res.render('moto-x3m-winter');
-});
-
+for (const [key, value] of Object.entries(sourceMap)) {
+  app.get(`/${key}`, (req, res) => {
+    res.render(value);
+  });
+}
 
 app.listen(port, async () => {
-
-
   console.log(`Server started on port ${port}`);
 });
 //https://wai.137900.xyz/search?q=test
