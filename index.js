acd951320ba5d83143b87e072633bb08b577cf9a
p0syd0n <ajeff9752@gmail.com>
2024-05-16 16:00:23 +0000

    miles is unfucked

--- index.js
+++ index.js
@@ -51,9 +51,6 @@ app.set('views', path.join(__dirname, 'public', 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("*", (req, res) => {
-  res.send('fuck you miles');
-})
 
 //static routes
 app.get('/', (req, res) => {
